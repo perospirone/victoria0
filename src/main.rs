47c17bd31f3d58a4f63969cd6fadfd98ecab9b66
perospirone <danidevpy@gmail.com>
2025-04-04 20:09:49 -0300

    pop consumption

--- src/main.rs
+++ src/main.rs
@@ -102,6 +102,14 @@ pub struct Province {
     pub owner: String,
 }
 
+pub enum Culture {
+    Brazilian,
+}
+
+pub enum Religion {
+    Catholic,
+}
+
 pub enum PopClass {
     Farmers,
     Laborers,
@@ -116,15 +124,15 @@ pub struct PopGroup {
     pub id: u32,
     pub size: u32,
     pub class: PopClass,
-    pub needs: Vec<(GoodType, f64)>, // goods needed
+    pub culture: Culture,
+    pub religion: Religion,
+    pub literacy: f32,        // 0.0 - 1.0
+    pub happiness: f32,       // 0.0 - 1.0
+    pub political_power: f32, // 0.0 - 1.0
+    pub money: f32,
+    pub needs: Vec<(GoodType, f32)>, // goods needed(good_type, quantity per 1000 pops)
+    pub province_id: i32,
 }
-
-//#[derive(Component)]
-//pub struct Population {
-//pub size: u32,
-//pub class: PopClass,
-//pub needs: Vec<(GoodType, f64)>, // goods needed
-//}
 
 fn get_base_price(good: GoodType) -> f32 {
     match good {
@@ -172,6 +180,22 @@ fn add_factories(mut commands: Commands) {
             output_goods: vec![(GoodType::Wine, 1.0)],
         },
     ));
+}
+
+fn add_pops(mut commands: Commands) {
+    commands.spawn(PopGroup {
+        id: 1,
+        size: 1000,
+        class: PopClass::Laborers,
+        money: 0.0,
+        needs: vec![(GoodType::Wine, 0.1), (GoodType::Grain, 0.2)],
+        culture: Culture::Brazilian,
+        religion: Religion::Catholic,
+        literacy: 0.2,
+        happiness: 1.0,
+        political_power: 0.1,
+        province_id: 1,
+    });
 }
 
 fn setup(mut commands: Commands) {}
@@ -248,8 +272,43 @@ fn update_prices_system(mut market: ResMut<Market>, mut new_day_ev: EventReader<
 
 fn population_consumption_system(
     mut market: ResMut<Market>,
+    mut query: Query<&PopGroup>,
     mut new_day_ev: EventReader<NewDayEvent>,
 ) {
+    if !new_day_ev.is_empty() {
+        new_day_ev.clear(); // clean processed events
+
+        for pop in query.iter() {
+            for (good_type, quantity) in &pop.needs {
+                let demand = quantity * pop.size as f32 / 1000 as f32;
+                println!(
+                    "good_type: {:?}, quantity: {:?}, demand: {:?}",
+                    good_type, quantity, demand
+                );
+
+                if let Some((_, available_quantity, _)) =
+                    market.goods.iter_mut().find(|(gt, _, _)| gt == good_type)
+                {
+                    if *available_quantity >= demand {
+                        *available_quantity -= demand;
+                        println!("Pop {} consumiu {:.4} de {:?}", pop.id, demand, good_type);
+                    } else {
+                        println!(
+                            "Pop {} queria {:.4} de {:?} mas só tinha {:.4}",
+                            pop.id, demand, good_type, available_quantity
+                        );
+                        // talvez consumir só o que está disponível:
+                        *available_quantity = 0.0;
+                    }
+                } else {
+                    println!(
+                        "Pop {} queria {:?}, mas não havia esse item no mercado!",
+                        pop.id, good_type
+                    );
+                }
+            }
+        }
+    }
 }
 
 fn main() {
@@ -266,11 +325,17 @@ fn main() {
         .insert_resource(Market { goods })
         .add_systems(
             Startup,
-            (setup, add_goods, add_provincies, add_factories).chain(),
+            (setup, add_goods, add_provincies, add_factories, add_pops).chain(),
         )
         .add_systems(
             Update,
-            (production_system, update_prices_system, advance_time).chain(),
+            (
+                production_system,
+                population_consumption_system,
+                update_prices_system,
+                advance_time,
+            )
+                .chain(),
         )
         .run();
 }
